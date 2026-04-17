2288d741cab0cea41906b93086da3a409c924cf4
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-04-17 16:01:35 +0200

    Added Functions to song class with Log.v

--- app/src/main/java/com/example/musicapp/Song.kt
+++ app/src/main/java/com/example/musicapp/Song.kt
@@ -1,10 +1,39 @@
 package com.example.musicapp
 
+import android.util.Log
+
 class Song {
 
     //adding variables for song objects attributes
-    var name : String = " "
-    var artist : String = " "
-    var year : Int = 0
-    var duration : String = " "
+    var name: String = " "
+    var artist: String = " "
+    var year: Int = 0
+    var duration: String = " "
+
+    //function to play song
+    fun Play() {
+        Log.v("Song ", "$name is playing")
+
+    }
+
+    //function to pause song
+    fun Pause(){
+        Log.v("Song", "$name is paused")
+    }
+
+    //function to stop song
+    fun Stop(){
+        Log.v("Song", "$name stopped now")
+    }
+
+    //function to favourite song
+    fun Favourite(): Boolean{
+        Log.v("Song", "$name is favourited now")
+        //return placeholder
+        return true
+
+
+    }
+
+
 }
