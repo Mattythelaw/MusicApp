24793ddf43b4f8bd5f1b8982477d915aeb8e1028
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-04-17 15:51:34 +0200

    Added song class variables

---
+++ app/src/main/java/com/example/musicapp/Song.kt
@@ -0,0 +1,10 @@
+package com.example.musicapp
+
+class Song {
+
+    //adding variables for song objects attributes
+    var name : String = " "
+    var artist : String = " "
+    var year : Int = 0
+    var duration : String = " "
+}
